43a937de733b5d60bb4829b4ee6836ea381e6ff0
AdongoNat <danieladongonat@gmail.com>
2025-03-01 23:51:39 +0000

    server

--- week1/server.js
+++ week1/server.js
@@ -1,10 +1,18 @@
 const express = require('express');
+
+const mongodb = require('./data/database');
 const app = express();
- 
-app.get('/', (req, res) => {
-  res.send("Hello");
-});
- 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
+
+const port = process.env.PORT || 3000;
+
+app.use('/', require('./routes'));
+
+mongodb.initDb((err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        app.listen(port, () => {
+            console.log(`Database  is listening and node Running on port ${port}`);
+        });
+    }
 });
