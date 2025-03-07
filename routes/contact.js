fa52a6ed8c9925d75406f79d2b521e9adbeedff7
AdongoNat <danieladongonat@gmail.com>
2025-03-07 15:07:07 +0000

    CRUD

--- routes/contact.js
+++ routes/contact.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const contactController = require('../controllers/contact');
 
-const contactsController = require('../controllers/contact');
-
-router.get('/', contactsController.getAll);
-
-router.get('/:id', contactsController.getSingle);
+router.get('/', contactController.getAll);
+router.get('/:id', contactController.getSingle);
+router.post('/', contactController.createContact);
+router.put('/:id', contactController.updateContact);
+router.delete('/:id', contactController.deleteContact);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,18 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('./data/database');
 
-const mongodb = require('./data/database')
 const app = express();
-
-
 const port = process.env.PORT || 8080;
+
+// Middleware
+app.use(bodyParser.json());  // Parses JSON body
+app.use(bodyParser.urlencoded({ extended: true })); // Parses URL-encoded data
+
+// Routes
 app.use('/', require('./routes'));
 
-
+// Connect to MongoDB and Start Server
 mongodb.initDb((err, mongodb) => {
     if (err) {
         console.log(err);
     } else {
-        app.listen(port);
-        console.log(`Connected to DB and listening on ${port}`);
+        app.listen(port, () => {
+            console.log(`Connected to DB and listening on ${port}`);
+        });
     }
 });
